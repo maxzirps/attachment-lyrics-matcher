1ffd6ac44bdc09857262daf0ee01ca8d5075ec28
Maximilian Zirps <hello+github@maxzirps.at>
2024-12-09 17:06:54 +0100

    improve model response format parsing; set temperature

--- backend/src/model.py
+++ backend/src/model.py
@@ -16,6 +16,8 @@ class TextGenerationModel:
             model=model_id,
             torch_dtype=torch.bfloat16,
             device_map="auto",
+            # TODO: make it more deterministic
+            temperature=0.01
         )
         
     def classify_attachment_style(self, lyrics: str, max_new_tokens: int = 256) -> AttachmentStyleProbabilities:
@@ -24,7 +26,7 @@ class TextGenerationModel:
         Analyze the following song lyrics and classify the attachment style it reflects. 
         Use the four main attachment styles: secure, anxious, avoidant, and disorganized. 
         Provide the confidence/probability for each style. The total of all probabilities should sum to 1.
-        Do not include any commentary, explanations, or additional text.
+        Do not include any commentary, explanations, or additional text. Return the probabilites as float.
         """
 
         messages = [
@@ -33,12 +35,13 @@ class TextGenerationModel:
         ]
         
         outputs = self.pipe(messages, max_new_tokens=max_new_tokens)
-        
-        matches = re.findall(r"(Secure|Anxious|Avoidant|Disorganized): (\d+)%", outputs[0]["generated_text"][-1]["content"])
+        answer = outputs[0]["generated_text"][-1]["content"]
+        print(answer)
+        matches = re.findall(r"(Secure|Anxious|Avoidant|Disorganized): (\d+\.\d+)", answer)
 
 
         attachment_probs: AttachmentStyleProbabilities = {
-            match[0].lower(): float(match[1]) / 100 for match in matches
+            match[0].lower(): float(match[1]) for match in matches
             }
 
 
